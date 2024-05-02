b3f0f9c9983d086132d8c5fd3fe57275f7f38e49
Shinton George <shintonseg5@gmail.com>
2024-05-03 02:23:12 +0530

    updated login service and auth service, backend -cross origin

--- Task_front_end/src/app/auth/components/login/login.component.ts
+++ Task_front_end/src/app/auth/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +13,10 @@ export class LoginComponent {
   loginForm ! : FormGroup;
   hidePassword = true;
 
-  constructor(private formBuilder : FormBuilder) {
+  constructor(private formBuilder : FormBuilder,
+    private authService : AuthService,
+    private snackbar : MatSnackBar
+  ) {
     this.loginForm = this.formBuilder.group({
       email: [null, [Validators.required, Validators.email]],
       password: [null, [Validators.required]],
@@ -24,6 +29,14 @@ export class LoginComponent {
 
   onSubmit(){
     console.log(this.loginForm.value);
+    this.authService.login(this.loginForm.value).subscribe((res)=>{
+      console.log(res);
+      if(res.id != null){
+        this.snackbar.open('Login successful', 'Close', {duration:5000});
+      }else{
+        this.snackbar.open('Login failed, Try again', 'Close', {duration:5000, panelClass: 'error-snackbar'});
+      }
+    })
   }
 
 }
--- Task_front_end/src/app/auth/components/signup/signup.component.ts
+++ Task_front_end/src/app/auth/components/signup/signup.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -10,7 +13,11 @@ export class SignupComponent {
   signupForm! : FormGroup;
   hidePassword = true;
 
-  constructor(private formBuilder : FormBuilder) {
+  constructor(private formBuilder : FormBuilder,
+    private snackBar : MatSnackBar,
+    private authService: AuthService,
+    private router : Router
+  ) {
     this.signupForm = this.formBuilder.group({
       name: [null, [Validators.required]],
       email: [null, [Validators.required, Validators.email]],
@@ -25,5 +32,20 @@ export class SignupComponent {
 
   onSubmit(){
     console.log(this.signupForm.value);
+    const password = this.signupForm.get('password')?.value;
+    const confirmPassword = this.signupForm.get('confirmPassword')?.value;
+    if(password!==confirmPassword){
+      this.snackBar.open('Password do not match','Close',{duration:5000, panelClass:'error-snackbar'});
+      return;
+    }
+    this.authService.signup(this.signupForm.value).subscribe(res=>{
+      console.log(res);
+      if(res.id != null){
+        this.snackBar.open('Signup Successful','Close', {duration:5000})
+        this.router.navigateByUrl("/login");
+      }else{
+        this.snackBar.open("Signup failed !!! Try again","Close", {duration:5000, panelClass: 'error-snackbar'});
+      }
+    })
   }
 }
