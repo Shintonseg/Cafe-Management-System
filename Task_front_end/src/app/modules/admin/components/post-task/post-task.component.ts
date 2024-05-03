5a46422cbf811f6994b3a15a5a1f0ce269a85029
Shinton George <shintonseg5@gmail.com>
2024-05-04 02:22:09 +0530

    post task elements added

--- Task_front_end/src/app/modules/admin/components/post-task/post-task.component.ts
+++ Task_front_end/src/app/modules/admin/components/post-task/post-task.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../services/admin.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDatepickerControl, MatDatepickerPanel } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-post-task',
@@ -7,14 +9,32 @@ import { AdminService } from '../../services/admin.service';
   styleUrls: ['./post-task.component.scss']
 })
 export class PostTaskComponent {
+  
+  taskForm!: FormGroup;
+  listOfEmployees: any = [];
+  listOfPriorities: any = ["LOW", "MEDIUM", "HIGH"];
 
-  constructor(private adminService: AdminService){
+  constructor(private adminService: AdminService,
+    private formBuilder: FormBuilder
+  ){
     this.getUsers();
+    this.taskForm = this.formBuilder.group({
+      employeeId: [null, Validators.required] ,
+      title: [null, Validators.required],
+      description: [null, Validators.required],
+      dueDate: [null, Validators.required],
+      priority: [null, Validators.required]
+    })
   }
 
   getUsers(){
     this.adminService.getUsers().subscribe(res=>{
       console.log(res);
+      this.listOfEmployees=res;
     })
+  }
+
+  postTask(){
+    console.log(this.taskForm.value);
   }
 }
